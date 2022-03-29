71e210cd37623f1a158c9f2386460d05479760c4
42hena <somnium1108@gmail.com>
2022-03-29 22:18:27 +0900

    Change : no use print function

--- monitor.c
+++ monitor.c
@@ -5,18 +5,23 @@ void *monitor(void *data)
     t_philos        *philo;
     struct timeval  now;
     long long       ms;
+    const char *state_str[5] = \
+		{"is thinking", "has taken a fork", \
+		"is eating", "is sleeping", "is dead"};
 
     philo = (t_philos *)data;
-    while(!philo->gameinfo->end)
+    while(!philo->gameinfo->end && philo->state != DEAD)
     {
         pthread_mutex_lock(&philo->gameinfo->work_mutex);
         gettimeofday(&now, NULL);
         ms = get_time_ms(now) - get_time_ms(philo->recent_eat_time);
-        if (ms >= philo->gameinfo->died_time)
+        if (ms >= philo->gameinfo->died_time && !philo->gameinfo->end)
         {
             philo->gameinfo->end = 1;
-            print_message_with_ms(philo, DEAD, ms);
-            break;
+            philo->state = DEAD;
+            if (philo->gameinfo->end)
+		        printf("ms: %lld index:%d %s\n", ms, philo->index,state_str[DEAD]);
+            // print_message_with_ms(philo, DEAD, ms);
         }
         pthread_mutex_unlock(&philo->gameinfo->work_mutex);
     }
