ca177e6ecda2be47c7f57120e2ea93c17cf1f379
42hena <somnium1108@gmail.com>
2022-03-31 17:52:41 +0900

    Change : add and remove
    - add 42Header
    - delete print_message_ms function

--- print_message.c
+++ print_message.c
@@ -1,26 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_message.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hena <hena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 16:57:48 by hena              #+#    #+#             */
+/*   Updated: 2022/03/31 16:57:48 by hena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philos.h"
 
-void print_message_with_ms(t_philos *philoinfo, int type, long long ms)
-{
-	const char *state_str[5] = \
-		{"is thinking", "has taken a fork", \
-		"is eating", "is sleeping", "is dead"};
-	pthread_mutex_lock(&philoinfo->gameinfo->work_mutex);
-	if (!philoinfo->gameinfo->end)
-		printf("ms: %lld index:%d %s\n", ms, philoinfo->index,state_str[type]);
-	pthread_mutex_unlock(&philoinfo->gameinfo->work_mutex);
-}
-
-void print_message(t_philos *philoinfo, int type)
+void	print_message(t_philos *philoinfo, int type)
 {
 	struct timeval		now;
-	const char *state_str[5] = \
+	const char			*state_str[5] = \
 		{"is thinking", "has taken a fork", \
 		"is eating", "is sleeping", "is dead"};
 
-	pthread_mutex_lock(&philoinfo->gameinfo->work_mutex);
+	pthread_mutex_lock(&philoinfo->gameinfo->end_mutex);
 	gettimeofday(&now, NULL);
 	if (!philoinfo->gameinfo->end)
-		printf("ms: %lld index:%d %s\n", get_time_ms(now) - get_time_ms(philoinfo->gameinfo->create_time), philoinfo->index,state_str[type]);
-	pthread_mutex_unlock(&philoinfo->gameinfo->work_mutex);
+		printf("%-10lldms\tphilos [%3d] %s\n", \
+			get_time_ms(now) - get_time_ms(philoinfo->gameinfo->create_time), \
+			philoinfo->index, state_str[type]);
+	pthread_mutex_unlock(&philoinfo->gameinfo->end_mutex);
 }
