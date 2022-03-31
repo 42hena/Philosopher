60dee5f0ac5aaa32eb8c0b343eea825f382c11b5
42hena <somnium1108@gmail.com>
2022-03-31 17:55:14 +0900

    Feat : add eating function
    - divide do_eat function to do_eat and eating function

--- philos_state.c
+++ philos_state.c
@@ -1,49 +1,48 @@
 #include "philos.h"
 
-void do_pick(t_philos *philoinfo)
+void	do_pick(t_philos *philoinfo)
 {
 	pthread_mutex_lock(philoinfo->leftfork);
-	philoinfo->state = PICKING;
-	print_message(philoinfo, philoinfo->state);
+	print_message(philoinfo, PICKING);
 	pthread_mutex_lock(philoinfo->rightfork);
-	print_message(philoinfo, philoinfo->state);
+	print_message(philoinfo, PICKING);
 }
 
-void do_eat(t_philos *philoinfo)
+static void	eating(t_philos *philoinfo)
 {
 	long long	ms;
-	const char *state_str[5] = \
+	const char	*state_str[5] = \
 		{"is thinking", "has taken a fork", \
 		"is eating", "is sleeping", "is dead"};
 
-	philoinfo->state = EATING;
 	gettimeofday(&philoinfo->recent_eat_time, NULL);
-	ms = get_time_ms(philoinfo->recent_eat_time) - get_time_ms(philoinfo->gameinfo->create_time);
-	pthread_mutex_lock(&philoinfo->gameinfo->work_mutex);
+	ms = get_time_ms(philoinfo->recent_eat_time) - \
+	get_time_ms(philoinfo->gameinfo->create_time);
 	if (!philoinfo->gameinfo->end)
 	{
-		printf("ms: %lld index:%d %s\n", ms, philoinfo->index,state_str[philoinfo->state]);
+		printf("%-10lldms\tphilos [%3d] %s\n", \
+				ms, philoinfo->index, state_str[EATING]);
+		philoinfo->eat_count++;
 	}
-	pthread_mutex_unlock(&philoinfo->gameinfo->work_mutex);
+}
 
-	// usleep(philoinfo->gameinfo->eating_time * 1000);
+void	do_eat(t_philos *philoinfo)
+{
+	pthread_mutex_lock(&philoinfo->gameinfo->end_mutex);
+	eating(philoinfo);
+	pthread_mutex_unlock(&philoinfo->gameinfo->end_mutex);
 	sleep_function(philoinfo->gameinfo->eating_time);
-
 	pthread_mutex_unlock(philoinfo->leftfork);
 	pthread_mutex_unlock(philoinfo->rightfork);
 }
 
-void do_sleep(t_philos *philoinfo)
+void	do_sleep(t_philos *philoinfo)
 {
-	philoinfo->state = SLEEPING;
-	print_message(philoinfo, philoinfo->state);
-	
-	usleep(philoinfo->gameinfo->sleeping_time * 1000);
-	//sleep_function(philoinfo->gameinfo->sleeping_time);
+	print_message(philoinfo, SLEEPING);
+	sleep_function(philoinfo->gameinfo->sleeping_time);
 }
 
-void do_think(t_philos *philoinfo)
+void	do_think(t_philos *philoinfo)
 {
-	philoinfo->state = THINKING;
-	print_message(philoinfo, philoinfo->state);
+	print_message(philoinfo, THINKING);
 }
