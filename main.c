8e26969f7ccf2ea050d626d90c76668c814536b4
hena <somnium1108@gmail.com>
2023-08-13 03:47:26 +0900

    [Feat] argument value valid condition
    - check all argument value is valid. if it is invalid, print index,
      string and main thread will terminate.

--- main.c
+++ main.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
+#include "utility.h"
+
 
 int	main(int argc, char **argv)
 {
+	int i;
+
 	// check arguments count if argc is not 5 or 6
 	if (argc != 5 && argc != 6)
 	{
 		printf("The argc is not valid.\n");
 		return (1);
+	}
+
+	for (i = 1 ; i < argc ; ++i)
+	{
+		if (is_numeric_string(argv[i]) == 0)
+		{
+			printf("The value at index %d is not a number. That string value is '%s'.\n", i, argv[i]);
+			return (1);
+		}
 	}
 
 	return (0);
