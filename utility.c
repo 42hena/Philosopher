6eed9d40c1db0e16a2290e3cd86e6bede3ad2755
hena <somnium1108@gmail.com>
2023-08-15 17:18:23 +0900

    [Comment] Add input output
    - described input and output without adding an explanation.

--- utility.c
+++ utility.c
@@ -1,9 +1,23 @@
+/*
+	input :	const char ch
+	output:	true or false
+
+	description
+*/
+
 int is_number(const char ch)
 {
 	if (ch >= '0' && ch <= '9')
 		return (1);
 	return (0);
 }
+
+/*
+	input :	const char *str
+	output:	true or false
+
+	description
+*/
 
 int is_numeric_string(const char *str)
 {
