f504b894e63ff0cb2926544515d7cab6c4ac85cc
hena <somnium1108@gmail.com>
2023-08-16 00:30:21 +0900

    [Faet] represent philo's state
    - do pick, do eat, do sleep, do think

---
+++ philo_state.h
@@ -0,0 +1,10 @@
+#ifndef __PHILO_STATE_H__
+#define __PHILO_STATE_H__
+
+void	do_pick(t_philo *philoinfo);
+void	eating(t_philo *philoinfo);
+void	do_eat(t_philo *philoinfo);
+void	do_sleep(t_philo *philoinfo);
+void	do_think(t_philo *philoinfo);
+
+#endif
