929e46d1ad3c7aa705ced982f444d0da8c9287b1
hena <somnium1108@gmail.com>
2023-08-18 01:51:30 +0900

    [Feat] previous function

---
+++ philo_process/utility.c
@@ -0,0 +1,55 @@
+#include <unistd.h>
+#include <sys/time.h>
+
+/*
+	input :	const char ch
+	output:	true or false
+
+	description
+*/
+
+int is_number(const char ch)
+{
+	if (ch >= '0' && ch <= '9')
+		return (1);
+	return (0);
+}
+
+/*
+	input :	const char *str
+	output:	true or false
+
+	description
+*/
+
+int is_numeric_string(const char *str)
+{
+	while (*str)
+	{
+		if (is_number(*str) == 0)
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+long	get_now_ms(void)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+}
+
+long	get_time_ms(struct timeval not_ms)
+{
+	long long	ms;
+
+	ms = not_ms.tv_sec * 1000 + not_ms.tv_usec / 1000;
+	return (ms);
+}
+
+void	run_sleep(int time)
+{
+	usleep(time * 1000);
+}
---
+++ philo_process/utility.h
@@ -0,0 +1,10 @@
+#ifndef __utility_h__
+#define __utility_h__
+
+int is_numeric_string(const char *str);
+
+long get_now_ms(void);
+long get_time_ms(struct timeval not_ms);
+void run_sleep(int time);
+
+#endif
