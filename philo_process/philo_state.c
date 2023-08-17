4e9e3be94564455d065229b487cd679ee3e8c3f5
hena <somnium1108@gmail.com>
2023-08-18 04:38:06 +0900

    [Feat] implements print_message, pick_forks
    - created a function that outputs differently depending on the type value.
    - pick_fork: takes one fork in one hand.
    - do_pick_forks: Pick up forks using pick_fork.

--- philo_process/philo_state.c
+++ philo_process/philo_state.c
@@ -9,15 +9,47 @@
 
 extern t_philo_info g_info;
 
+void print_message(t_philo *philo, int type)
+{
+	struct timeval		now;
+	const char			*state_str[] = \
+		{"is thinking", "has taken a left fork", \
+		"has taken a right fork",
+		"is eating", "is sleeping", "is dead"};
+	const long long start_time = g_info.start_time;
+
+	// lock sem_print
+	ret = sem_wait(philo->sem_print);
+	if (ret < 0)
+	{
+		printf("sem_wait Failed:%d\n", errno);
+		return ;
+	}
+
+	gettimeofday(&now, NULL);
+	if (!g_philo_info.end_flag)
+		printf("%-10lldms\tphilos [%3d] %s\n", \
+			get_time_ms(now) - start_time, \
+			philo->id, state_str[type]);
+	
+	// unlock sem_print
+	ret = sem_post(philo->sem_print);
+	if (ret < 0)
+	{
+		printf("sem_post Failed:%d\n", errno);
+		return ;
+	}
+}
+
 /*
-	input : [t_philo *]
+	input : [t_philo *, int]
 	output: [TRUE or FALSE]
 
 	This function picks up the fork in the middle of the table.
 	Fork is the number of semaphores, and returns 1 in case of success and 0 in case of failure.
 */
 
-int	do_pick(t_philo *philo)
+int	pick_fork(t_philo *philo, int type)
 {
 	int ret;
 
@@ -27,12 +59,30 @@ int	do_pick(t_philo *philo)
 		printf("sem_wait Failed:%d\n", errno);
 		return (0);
 	}
-	printf("pid[%3d] has a fork\n", number);
-	ret = sem_wait(philo->sem_fork);
+	print_message(philo, type);
+	return (1);
+}
+
+/*
+	input : [t_philo *]
+	output: [TRUE or FALSE]
+
+	This function picks up the fork in the middle of the table.
+	Fork is the number of semaphores, and returns 1 in case of success and 0 in case of failure.
+*/
+
+int	do_pick_forks(t_philo *philo)
+{
+	int ret;
+
+	// pick left fork
+	ret = pick_fork(philo, PICKING_LEFT_FORK);
 	if (ret < 0)
-	{
-		printf("sem_wait Failed:%d\n", errno);
 		return (0);
-	}
+	
+	// pick right fork
+	ret = pick_fork(philo, PICKING_RIGHT_FORK);
+	if (ret < 0)
+		return (0);
 	return (1);
 }
