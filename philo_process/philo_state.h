5b85f55ffdc05407b7d8e8fa84e16e5c26f2118f
hena <somnium1108@gmail.com>
2023-08-18 12:50:25 +0900

    [Fix] occur error
    - The ";" is missing after the function in header file.
    - An argument error in the do_eat function was found.

--- philo_process/philo_state.h
+++ philo_process/philo_state.h
@@ -1,7 +1,7 @@
 #ifndef __PHILO_STATE_H__
 #define __PHILO_STATE_H__
 
-int     do_pick_forks(t_philo *philo)
-void	do_eat(t_philo *philoinfo);
+int     do_pick_forks(t_philo *philo);
+void	do_eat(t_philo *philo);
 
 #endif
