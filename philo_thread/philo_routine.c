faec8db518926863f370aa4e7251f2e51d9db491
42hena <somnium1108@gmail.com>
2023-09-10 23:01:20 +0900

    [Feat] 데드락 검출 추가
    - [EDEADLK]을 사용하기 위해 <errno.h> 추가
    - pthread_join에서 데드락 검출문 추가

--- philo_thread/philo_routine.c
+++ philo_thread/philo_routine.c
@@ -3,6 +3,8 @@
 #include <sys/time.h>
 #include <pthread.h>
 
+#include <errno.h> // [EDEADLK]
+
 #include "philos.h"
 #include "philo_state.h"
 #include "utility.h"
@@ -70,6 +72,11 @@ void begin_philosopher_routines()
 	int i;
 	int philo_number;
 
+	// return variable
+	int pthread_join_ret;
+// -----
+
+
 	philo_number = g_dining_info.number_of_philos;
 
 	printf("Philosopher problem start\n");
@@ -85,9 +92,16 @@ void begin_philosopher_routines()
 		monitor_philos();
 	}
 
+	// main을 제외한 스레드를이 끝나기를 기다리는 코드임.
 	for (i = 0 ; i < philo_number ; ++i)
 	{
-		pthread_join(g_dining_info.philo_list[i].philo, NULL); // TODO: Add join return value for finding DEAD_LOCK
+		// 스레드의 작동을 기다림.
+		pthread_join_ret = pthread_join(g_dining_info.philo_list[i].philo, NULL);
+		if (pthread_join_ret == EDEADLK)
+		{
+			printf("The [%d]th philosopher thread is deadlocked.\n", i);
+			break ;
+		}
 	}
 
 	printf("Philosopher problem end\n");
